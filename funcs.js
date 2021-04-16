11173eff67bd56787011f1fadea4fde734cff1b8
Alex-Nav <alejugger@gmail.com>
2021-04-16 18:47:10 -0500

    basic book

--- funcs.js
+++ funcs.js
@@ -1,47 +1,38 @@
-console.log("hello");
+  
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#number');
+const readInput = document.querySelector('#finished');
 
-function Player(name, marker) {
-    this.name = name
-    this.marker = marker
-    this.sayName = function () {
-        console.log(name)
-    }
+const submitBtn = document.getElementById('submitBtn');
+
+let myLibrary = [];
+
+
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = Number(pages);
+    this.isRead = isRead;
 }
 
-const player1 = new Player('steve', 'X')
-const player2 = new Player('also steve', 'O')
-player1.sayName() // logs 'steve'
-player2.sayName() // logs 'also steve'
-
-function Book(title, autor, pages, isRead) {
-    this.title = title
-    this.autor = autor
-    this.pages = pages
-    this.isRead = isRead
+function addBookToLibrary() {
+    titulo =  titleInput.value;
+    autor =  titleInput.value;
+    paginas = pagesInput.value;
+    leido = readInput.checked;
+    let newBook = new Book(titulo, autor, paginas, leido)
+    myLibrary.push(newBook)
 }
 
-Book.prototype.sayBook = function () {
-    console.log(this.title + " is a book written by " + this.autor);
 
-    if (this.isOld) {
-        console.log("es un libro viejo");
-    } else {
-        console.log("es un libro nuevo");
-    }
+function showBooksInLibrary() {
+    myLibrary.forEach(element => {
+        console.log(" el libro " + element.title);
+    });
 }
 
-function LibroAntiguo(name, autorrrr) {
-    this.title = name;
-    this.autor = autorrrr;
-    this.isOld= true;
-}
+submitBtn.addEventListener('click', addBookToLibrary);
 
-LibroAntiguo.prototype = Object.create(Book.prototype)
 
-const book2 = new LibroAntiguo("La Odisea" , "Homero")
-
-book2.sayBook()
-
-const book1 = new Book('hobbit', "JRR Tolkien ", 342, false)
-
-book1.sayBook()
